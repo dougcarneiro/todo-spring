503c71aecf751c6d30e6162f6bd8e122cea4ea40
Douglas Carneiro <douglas.ul@gmail.com>
2025-03-18 22:32:35 -0300

    usando response entity

--- src/main/java/com/todo_app/api/controllers/TaskController.java
+++ src/main/java/com/todo_app/api/controllers/TaskController.java
@@ -4,6 +4,8 @@ import com.todo_app.api.models.Task;
 import com.todo_app.api.services.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 
 import java.util.List;
 
@@ -15,10 +17,10 @@ public class TaskController {
     private TodoService todoService;
 
     @GetMapping()
-    public List<Task> findAllByDonoId(
+    public ResponseEntity<List<Task>> findAllByDonoId(
             @RequestParam(required = true) String donoId,
             @RequestParam(required = true) boolean removido_ne,
-            @RequestParam(required = false) String search,
+            @RequestParam(required = false) String q,
             @RequestParam(required = false) List<String> status,
             @RequestParam(required = false) List<String> prioridade,
             @RequestParam(required = false) String _sort,
@@ -26,23 +28,28 @@ public class TaskController {
     ) {
         String sortField = _sort != null ? _sort : "dataCriacao";
         String sortOrder = _order != null ? _order : "desc";
+        String search = q != null ? "%" + q + "%" : "";
 
-        return todoService.findAllByDono(donoId, removido_ne, search, status, prioridade, sortField, sortOrder);
+        List<Task> tasks = todoService.findAllByDono(donoId, removido_ne, search, status, prioridade, sortField, sortOrder);
+        return new ResponseEntity<>(tasks, HttpStatus.OK);
     }
 
     @PostMapping
-    public Task inserir(@RequestBody Task task) {
-        return todoService.inserir(task);
+    public ResponseEntity<Task> inserir(@RequestBody Task task) {
+        Task newTask = todoService.inserir(task);
+        return new ResponseEntity<>(newTask, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    public Task atualizar(@RequestBody Task task) {
-        return todoService.atualizar(task);
+    public ResponseEntity<Task> atualizar(@RequestBody Task task) {
+        Task updatedTask = todoService.atualizar(task);
+        return new ResponseEntity<>(updatedTask, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public void remover(@PathVariable long id) {
+    public ResponseEntity<Void> remover(@PathVariable long id) {
         todoService.remover(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
